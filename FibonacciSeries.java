d7cfa038fa59904179f41c7363d630930d0d62aa
SmitaDugur <smitadugur25@gmail.com>
2021-12-15 02:18:56 +0530

    ADDED:Fibonacci Series

---
+++ FibonacciSeries.java
@@ -0,0 +1,31 @@
+package LogicalProblems;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    //private static int two;
+    fibonacciSeries(int num , int one , int two){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Numbers : ");
+        int n = sc.nextInt();
+        int first  = 0;
+        int second = 1;
+        findSeries(n ,first ,second);
+        System.out.print( "  "  );
+
+    }
+    static void findSeries(int num , int one , int two){
+        System.out.println( "  " +one);
+        int counter = 0, sum;
+        while (counter < num - 1 ){
+            sum = one + two;
+            System.out.println(" " + sum);
+            one = two;
+            two = sum;
+            counter++;
+        }
+    }
+}
